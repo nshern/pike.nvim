1bbf9610ba3d899cae944db82f60f13291152b0f
Niklas Alexander Shern <nshern@pm.me>
2026-05-14 21:39:45 +0200

    .

--- colors/pike.lua
+++ colors/pike.lua
@@ -1,60 +1,128 @@
 vim.cmd("hi clear")
 if vim.fn.exists("syntax_on") == 1 then
-    vim.cmd("syntax reset")
+	vim.cmd("syntax reset")
 end
 
 local palette = require("palette")
 
 vim.g.colors_name = "pike"
 
-vim.api.nvim_set_hl(0, "@variable", { link = Normal })
-vim.api.nvim_set_hl(0, "ColorColumn", { bg = palette.gray_90 })
-vim.api.nvim_set_hl(0, "Comment", { fg = palette.gray_60, bold = true })
-vim.api.nvim_set_hl(0, "Conceal", { bg = palette.gray_70 })
+local p = palette
+local c = vim.o.background == "light"
+		and {
+			fg = p.gray_100,
+			bg = p.white,
+			bg_dim = p.gray_10,
+			bg_float = p.gray_20,
+			bg_pmenu = p.gray_30,
+			bg_select = p.gray_40,
+			bg_visual = p.blue_20,
+			bg_search = p.yellow_20,
+			fg_search = p.black,
+			bg_cursearch = p.yellow_30,
+			fg_cursearch = p.black,
+			fg_statusline = p.gray_70,
+			bg_statusline = p.gray_10,
+			fg_linenr = p.gray_50,
+			fg_cursorlinenr = p.gray_70,
+			fg_nontext = p.gray_40,
+			fg_unnecessary = p.gray_40,
+			fg_comment = p.gray_60,
+			fg_error = p.red_60,
+			fg_warn = p.yellow_60,
+			fg_info = p.cyan_70,
+			fg_hint = p.blue_70,
+			fg_added = p.green_70,
+			fg_changed = p.blue_70,
+			fg_removed = p.red_60,
+			fg_dir = p.blue_70,
+			fg_match = p.magenta_70,
+			fg_more = p.teal_70,
+			fg_mode = p.green_70,
+			fg_question = p.cyan_70,
+			fg_warningmsg = p.yellow_60,
+		}
+	or {
+		fg = p.gray_30,
+		bg = p.black,
+		bg_dim = p.gray_100,
+		bg_float = p.gray_90,
+		bg_pmenu = p.gray_80,
+		bg_select = p.gray_70,
+		bg_visual = p.blue_80,
+		bg_search = p.yellow_70,
+		fg_search = p.white,
+		bg_cursearch = p.yellow_30,
+		fg_cursearch = p.black,
+		fg_statusline = p.gray_30,
+		bg_statusline = p.gray_100,
+		fg_linenr = p.gray_60,
+		fg_cursorlinenr = p.gray_70,
+		fg_nontext = p.gray_70,
+		fg_unnecessary = p.gray_70,
+		fg_comment = p.gray_60,
+		fg_error = p.red_40,
+		fg_warn = p.yellow_40,
+		fg_info = p.cyan_40,
+		fg_hint = p.blue_40,
+		fg_added = p.green_40,
+		fg_changed = p.blue_40,
+		fg_removed = p.red_40,
+		fg_dir = p.blue_40,
+		fg_match = p.magenta_60,
+		fg_more = p.teal_40,
+		fg_mode = p.green_40,
+		fg_question = p.cyan_50,
+		fg_warningmsg = p.yellow_30,
+	}
+
+vim.api.nvim_set_hl(0, "@variable", { link = "Normal" })
+vim.api.nvim_set_hl(0, "ColorColumn", { bg = c.bg_float })
+vim.api.nvim_set_hl(0, "Comment", { fg = c.fg_comment, bold = true })
+vim.api.nvim_set_hl(0, "Conceal", { bg = c.bg_select })
 vim.api.nvim_set_hl(0, "Constant", {})
-vim.api.nvim_set_hl(0, "CurSearch", { bg = palette.yellow_30, fg = palette.black })
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = palette.gray_70 })
-vim.api.nvim_set_hl(0, "Cursorline", { bg = palette.gray_100 })
-vim.api.nvim_set_hl(0, "Delimiter", { link = Normal })
-vim.api.nvim_set_hl(0, "DiagnosticError", { fg = palette.red_40 })
-vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = palette.blue_40 })
-vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = palette.cyan_40 })
-vim.api.nvim_set_hl(0, "DiagnosticSignError", { fg = palette.red_40 })
-vim.api.nvim_set_hl(0, "DiagnosticSignHint", { fg = palette.blue_40 })
-vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { undercurl = true, sp = red_40 })
-vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { undercurl = true, sp = palette.yellow_40 })
-vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { fg = palette.gray_70 })
-vim.api.nvim_set_hl(0, "DiagnosticWarn", { fg = palette.yellow_40 })
-vim.api.nvim_set_hl(0, "Directory", { fg = palette.blue_40 })
-vim.api.nvim_set_hl(0, "ErrorMsg", { fg = palette.red_40 })
-vim.api.nvim_set_hl(0, "Function", { link = Normal })
-vim.api.nvim_set_hl(0, "Identifier", { link = Normal })
-vim.api.nvim_set_hl(0, "Linenr", { fg = palette.gray_60 })
-vim.api.nvim_set_hl(0, "Changed", { fg = palette.blue_40 })
-vim.api.nvim_set_hl(0, "Added", { fg = palette.green_40 })
-vim.api.nvim_set_hl(0, "Removed", { fg = palette.red_40 })
-vim.api.nvim_set_hl(0, "MatchParen", { fg = palette.magenta_60, bold = true })
-vim.api.nvim_set_hl(0, "MoreMsg", { fg = palette.teal_40 })
-vim.api.nvim_set_hl(0, "ModeMsg", { fg = palette.green_40 })
-vim.api.nvim_set_hl(0, "NoneText", { fg = palette.gray_70 })
-vim.api.nvim_set_hl(0, "Normal", {})
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = palette.gray_90 })
-vim.api.nvim_set_hl(0, "Operator", { link = Normal })
-vim.api.nvim_set_hl(0, "Pmenu", { bg = palette.gray_80 })
-vim.api.nvim_set_hl(0, "Pmenusel", { bg = palette.gray_70, bold = true })
-vim.api.nvim_set_hl(0, "PreProc", { link = Normal })
-vim.api.nvim_set_hl(0, "Question", { fg = palette.cyan_50 })
-vim.api.nvim_set_hl(0, "Quickfixline", { link = Question })
-vim.api.nvim_set_hl(0, "Search", { bg = palette.yellow_70, fg = palette.white })
-vim.api.nvim_set_hl(0, "SignColumn", { link = Normal })
-vim.api.nvim_set_hl(0, "Special", { link = Normal })
-vim.api.nvim_set_hl(0, "SpecialKey", { bg = palette.gray_70 })
-vim.api.nvim_set_hl(0, "Spellbad", { link = DiagnosticUnderlineError })
-vim.api.nvim_set_hl(0, "Statement", { link = Normal })
-vim.api.nvim_set_hl(0, "Statusline", { fg = palette.gray_30, bg = palette.gray_100 })
-vim.api.nvim_set_hl(0, "String", { link = Normal })
+vim.api.nvim_set_hl(0, "CurSearch", { bg = c.bg_cursearch, fg = c.fg_cursearch })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = c.fg_cursorlinenr })
+vim.api.nvim_set_hl(0, "Cursorline", { bg = c.bg_dim })
+vim.api.nvim_set_hl(0, "Delimiter", { link = "Normal" })
+vim.api.nvim_set_hl(0, "DiagnosticError", { fg = c.fg_error })
+vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = c.fg_hint })
+vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = c.fg_info })
+vim.api.nvim_set_hl(0, "DiagnosticSignError", { fg = c.fg_error })
+vim.api.nvim_set_hl(0, "DiagnosticSignHint", { fg = c.fg_hint })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { undercurl = true, sp = c.fg_error })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { undercurl = true, sp = c.fg_warn })
+vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { fg = c.fg_unnecessary })
+vim.api.nvim_set_hl(0, "DiagnosticWarn", { fg = c.fg_warn })
+vim.api.nvim_set_hl(0, "Directory", { fg = c.fg_dir })
+vim.api.nvim_set_hl(0, "ErrorMsg", { fg = c.fg_error })
+vim.api.nvim_set_hl(0, "Function", { link = "Normal" })
+vim.api.nvim_set_hl(0, "Identifier", { link = "Normal" })
+vim.api.nvim_set_hl(0, "Linenr", { fg = c.fg_linenr })
+vim.api.nvim_set_hl(0, "Changed", { fg = c.fg_changed })
+vim.api.nvim_set_hl(0, "Added", { fg = c.fg_added })
+vim.api.nvim_set_hl(0, "Removed", { fg = c.fg_removed })
+vim.api.nvim_set_hl(0, "MatchParen", { fg = c.fg_match, bold = true })
+vim.api.nvim_set_hl(0, "MoreMsg", { fg = c.fg_more })
+vim.api.nvim_set_hl(0, "ModeMsg", { fg = c.fg_mode })
+vim.api.nvim_set_hl(0, "NoneText", { fg = c.fg_nontext })
+vim.api.nvim_set_hl(0, "Normal", { fg = c.fg, bg = c.bg })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = c.bg_float })
+vim.api.nvim_set_hl(0, "Operator", { link = "Normal" })
+vim.api.nvim_set_hl(0, "Pmenu", { bg = c.bg_pmenu })
+vim.api.nvim_set_hl(0, "Pmenusel", { bg = c.bg_select, bold = true })
+vim.api.nvim_set_hl(0, "PreProc", { link = "Normal" })
+vim.api.nvim_set_hl(0, "Question", { fg = c.fg_question })
+vim.api.nvim_set_hl(0, "Quickfixline", { link = "Question" })
+vim.api.nvim_set_hl(0, "Search", { bg = c.bg_search, fg = c.fg_search })
+vim.api.nvim_set_hl(0, "SignColumn", { link = "Normal" })
+vim.api.nvim_set_hl(0, "Special", { link = "Normal" })
+vim.api.nvim_set_hl(0, "SpecialKey", { bg = c.bg_select })
+vim.api.nvim_set_hl(0, "Spellbad", { link = "DiagnosticUnderlineError" })
+vim.api.nvim_set_hl(0, "Statement", { link = "Normal" })
+vim.api.nvim_set_hl(0, "Statusline", { fg = c.fg_statusline, bg = c.bg_statusline })
+vim.api.nvim_set_hl(0, "String", { link = "Normal" })
 vim.api.nvim_set_hl(0, "Title", { bold = true })
-vim.api.nvim_set_hl(0, "Title", { link = Normal })
-vim.api.nvim_set_hl(0, "Type", { link = Normal })
-vim.api.nvim_set_hl(0, "Visual", { bg = palette.blue_80 })
-vim.api.nvim_set_hl(0, "WarningMsg", { fg = palette.yellow_30 })
+vim.api.nvim_set_hl(0, "Type", { link = "Normal" })
+vim.api.nvim_set_hl(0, "Visual", { bg = c.bg_visual })
+vim.api.nvim_set_hl(0, "WarningMsg", { fg = c.fg_warningmsg })
